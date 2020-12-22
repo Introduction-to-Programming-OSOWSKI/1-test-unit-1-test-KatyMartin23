4dc2dd9f3fe2235084256cd80135cef6184d231e
KatyMartin23 <832006@mystma.org>
2020-12-22 16:25:20 +0000

    Unit 1 test try 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def name():
+    return "Katy"
+print (name())
+def favorite(favCol, favNum):
+    return "Your favorite number is " + favCol + " and your favorite number is " + favNum
+print (favorite("blue", 4))
+def bigAdd(x, y, z, a, b):
+    return x + y + z + a + b
+print (bigAdd(1, 2, 3, 4, 5,))
+def fourOperations(x, y, z, a, b):
+    return (x + y) - (z * a) / b
+print (fourOperations(2, 5, 1, 9, 5))
